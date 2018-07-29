afra/v4runa-shackie

commit e5a17e2c8c6529b04299a04d21b22b5bccbe19bc

afra: tell afrabot on every state chage

shackbot/plugins/afra.py
@@ -52,6 +52,7 @@ def say_state(state, target=None):
     else:
         for channel in AFRA_NOTIFICATION_CHANNELS:
             bot.say(channel, "The space is now %s." % human[state])
+            bot.say(target, "afrabot: %s!", human[state])
 
 async def check_state_change():
     ts_state, _ = get_space()
@@ -146,7 +147,6 @@ async def open_set(parsed, user, target, text):
     set_space(_OPEN)
     await check_state_change()
     bot.say(target, "Noted.")
-    bot.say(target, "afrabot: open!")
 
 @bot_command('close!')
 async def close_set(parsed, user, target, text):
@@ -158,7 +158,6 @@ async def closed_set(parsed, user, target, text):
     set_space(_CLOSED)
     await check_state_change()
     bot.say(target, "Noted.")
-    bot.say(target, "afrabot: close!")
 
 asyncio.ensure_future(wait_kick_space())
 asyncio.ensure_future(check_room_status())

commit ca7df220b3fdb23e62998618a9d22f79600ead36

afra.py: use bot.say() correctly to prevent an exception
Fixes: e5a17e2c8c65 ("afra: tell afrabot on every state chage")

shackbot/plugins/afra.py
@@ -52,7 +52,7 @@ def say_state(state, target=None):
     else:
         for channel in AFRA_NOTIFICATION_CHANNELS:
             bot.say(channel, "The space is now %s." % human[state])
-            bot.say(target, "afrabot: %s!", human[state])
+            bot.say(target, "afrabot: %s!" % human[state])
 
 async def check_state_change():
     ts_state, _ = get_space()